9297a9c72aeab37b77663d0c47617b78f98e145a
Adam Lindberg <ad.lindberg@gmail.com>
2023-08-07 20:22:34 +0200

    commit

--- speech_to_text.py
+++ speech_to_text.py
@@ -64,7 +64,7 @@ while(1):
     text = record_text()
     messages.append({"role":"user","content":text})
     response = send_to_chatGBT(messages)
-    SpeakText(response)
+    SpeakText(response,engine)
     print(response)
 
 
